94c6d30f7541be9a27e42535831741b00b50614e
Ben McDaniel <47250557+Ben-McDaniel@users.noreply.github.com>
2024-01-22 19:41:04 -0500

    asdf

--- exampleClient.c
+++ exampleClient.c
@@ -10,18 +10,16 @@
 #include <stdint.h>
 
 #define SERVER "137.112.38.47"
-// #define MESSAGE "0900760468691892"
 #define PORT 2324
 #define BUFSIZE 1024
-// #define VERSION 9
-// #define commID_RHMP 0x1874
-// #define commID_CTLMSG 118
+
 
 uint16_t fixed_checksum(char* buffer, int nBytes);
 void displayMessage(char* buffer, int nBytes);
 void printPayload(char* buffer, int length);
 char* getNextMessage();
 void messageSendSuccess();
+void printPayloadRHMP(char* buffer, int length);
 
 int messageCount = 1;
 
@@ -66,9 +64,28 @@ int main() {
         exit(0);
     }
 
-    char* message = getNextMessage();
+    uint8_t msg1[] = {0x09, 0xA8, 0x00, 0x05, 0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x00, 0xB2, 0x80};
+    uint8_t msg2[] = {0x09, 0x00, 0x76, 0x60, 0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x00, 0xCF, 0x6C};
+    uint8_t msg3[] = {0x09, 0x74, 0x18, 0x41, 0x31, 0x20, 0x76, 0x18, 0x37, 0x12};
+    uint8_t msg4[] = {0x09, 0x01, 0x18, 0x35, 0x31, 0x20, 0x76, 0x03, 0x37, 0xA6};
+
+
+
+    // if(messageCount == 1){
+    //     printf("Sending RHP Message: hello\n");
+    //     uint8_t msg1[] = {0x09, 0xA8, 0x00, 0x05, 0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x00, 0xB2, 0x80};
+    // }else if(messageCount == 2){
+    //     printf("Sending RHP Message: hi\n");
+    //     uint8_t msg2[] = {0x09, 0xA8, 0x00, 0x05, 0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x00, 0xB2, 0x80};
+    // }else if(messageCount == 3){
+    //     printf("Sending RHMP Message of type: Message_Request\n");
+    //     uint8_t msg3[] = {0x12, 0xE8, 0x30, 0x41, 0x31, 0x20, 0x76, 0x18, 0x15, 0x9E};
+    // }else{
+    //     printf("Sending RHMP Message of type: ID_Request\n");
+    //     uint8_t msg4[] = {0x12, 0xE8, 0x30, 0x41, 0x31, 0x20, 0x76, 0x18, 0x15, 0x9E};
+    // }
     
-    if (sendto(clientSocket, message, sizeof(message), 0,
+    if (sendto(clientSocket, msg4, sizeof(msg4), 0,
             (struct sockaddr *) &serverAddr, sizeof (serverAddr)) < 0) {
         perror("sendto failed");
         return 0;
@@ -84,48 +101,31 @@ int main() {
     return 0;
 }
 
-char* getNextMessage(){
-    uint8_t msg1[] = {0x09, 0xA8, 0x00, 0x05, 0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x00, 0xB2, 0x80};
-
-    uint8_t msg3[] = {0x12, 0xE8, 0x30, 0x41, 0x31, 0x20, 0x76, 0x18, 0x15, 0x9E};
-
-    if(messageCount == 1){
-        printf("Sending RHP Message: hello\n");
-        return msg1;
-    }else if(messageCount == 2){
-        printf("Sending RHP Message: hi\n");
-        return msg1;
-    }else if(messageCount == 3){
-        printf("Sending RHMP Message of type: Message_Request\n");
-        return msg3;
-    }else{
-        printf("Sending RHMP Message of type: ID_Request\n");
-        return msg3;
-    }
-}
-
 void messageSendSuccess(){
     messageCount += 1;
 }
 
 void displayMessage(char* buffer, int nBytes){
 
-    // for(int i = 0; i < nBytes; i+=2){
-    //     printf("%x%x\n",(uint8_t)buffer[i],(uint8_t)buffer[i+1]);
-    // }
     printf("==================================\n");
     printf("Message Recieved\n");
     printf("    RHP Version: %d\n",buffer[0]);
     printf("    commID: %u%u\n",(uint8_t)buffer[2],(uint8_t)buffer[1]);
 
-    uint8_t length = (uint8_t)buffer[3]>>1;
-    length = length<<1;
+    uint8_t length = (uint8_t)buffer[3]<<1;
+    length = length>>1;
     printf("    Length: %d\n", length);
 
-    uint8_t type = (uint8_t)buffer[3]&0b00000001;
+    uint8_t type = (uint8_t)buffer[3]&0b10000000;
     printf("    Type: %d\n",type);
 
-    printPayload(buffer, length);
+    if(type == 0){
+        //RHP message
+        printPayload(buffer, length);
+    }else{
+        //RHMP message
+        printPayloadRHMP(buffer, length);
+    }
 
     uint16_t messageChecksum = (uint16_t)buffer[nBytes-2]<<8;
     messageChecksum += (uint8_t)buffer[nBytes-1];
@@ -142,7 +142,7 @@ void displayMessage(char* buffer, int nBytes){
         messageSendSuccess();
     }
     
-    main();
+    // main();
 }
 
 void printPayload(char* buffer, int length){
@@ -152,6 +152,18 @@ void printPayload(char* buffer, int length){
         printf("%c", buffer[4+i]);
     }
     printf("\n");
+}
+
+void printPayloadRHMP(char* buffer, int length){
+    //know that the payload starts on buffer[4]
+
+    for(int i = 0; i < length; i+=2){
+        printf("\n%x %x\n", (uint8_t)buffer[i],(uint8_t)buffer[i+1]);
+    }
+    printf("    RHMP Protocol:\n");
+
+    int dstPort = (buffer[4]<<4) + (buffer[5]>>4);
+    printf("        dstPort: %d\n", dstPort);
 }
 
 uint16_t fixed_checksum(char* buffer, int nBytes){
@@ -167,365 +179,5 @@ uint16_t fixed_checksum(char* buffer, int nBytes){
     }
 
     sum = ~(uint16_t)sum;
-    // printf("checksum: %d",(uint16_t)sum);
     return sum;
 }
-
-
-
-
-
-// char* package_message(char* message){
-//     return message;
-// }
-
-
-
-// char* bufferToHexString(char* buffer, int bufferSize) {
-//     int resultSize = bufferSize * 2 + 1; 
-
-//     char *result = (char*)malloc(resultSize * sizeof(char));
-
-//     for (int i = 0; i < bufferSize; i++) {
-//         sprintf(result + i * 2, "%02X", buffer[i]);
-//     }
-
-
-//     result[resultSize - 1] = '\0';
-
-//     return result;
-// }
-
-
-
-// void unpackage_message(char message[]){
-//     //dont need to copy the buffer into new place?
-//     // size_t length = strlen(message);
-//     // char* result = (char*)malloc((length + 1) * sizeof(char));
-//     int bufferSize = sizeof(message) * 10 -1;
-
-//     // Convert the buffer to a hex string
-//     char* hexString = bufferToHexString(message, bufferSize);
-//     // printf("Hex string: %s\n", hexString);
-
-//     // printf("Message Recieved:\n");
-//     char version[2] = {hexString[0], hexString[1]};
-//     // printf("RHP Version: %02X\n", (int)version);
-
-//     int length = 4;
-//     for(int i = 8; i < 8+length; i+=2){
-//         char hexPair[2] = {hexString[i], hexString[i + 1]};
-//         char asciiChar = strtol(hexPair, NULL, 16);
-
-
-//         // printf("%c",(char)asciiChar);
-//     }
-//     displayReceived(hexString, bufferSize);
-//     free(hexString);
-// }
-
-
-
-// int getTwo(char message[], int index) {
-//     char tmp[17]; //+1 for /0
-
-//     for (int i = 0; i < 16; i++) {
-//         tmp[i] = message[index + i];
-//     }
-//     tmp[16] = '\0';
-
-//     return (int)strtol(tmp, NULL, 2);
-// }
-
-// int checkSum(char message[], int size) {
-//     uint32_t runningSum = 0;
-//     uint32_t previousSum = 0;
-
-//     for (int i = 0; i < size-8; i = i + 4) {
-//         // runningSum += getTwo(message, i);
-//         char tmp[4] = {'0','x', message[i], message[i+1]};
-//         char tmp2[4] = {'0','x', message[i+2], message[i+3]};
-//         // uint32_t tInt = ((uint16_t)strtol(tmp, NULL, 16)/16)<<8 + (uint16_t)strtol(tmp2, NULL, 16);
-//         uint32_t tInt = (((uint16_t)strtol(tmp, NULL, 16)/16)<<8) + (uint16_t)strtol(tmp2, NULL, 16);
-
-//         runningSum += tInt;
-//         // printf("%d\n",runningSum);
-
-//         runningSum = runningSum<<8;
-//         runningSum = runningSum>>8;
-//         // printf("%d\n\n",runningSum);
-//         if (previousSum > runningSum) {
-//             runningSum++;
-//         }
-//         previousSum = runningSum;
-        
-
-//     }
-//     // printf("%d",(uint16_t)runningSum);
-
-//     runningSum = ~runningSum;
-//     //off by constant factor of 100(decimal), seems to be off for multiple test cases, investigate further later
-//     return (uint16_t)runningSum+100;
-// }
-
-// int pow(int x,int y) {
-//     int tmp = 1;
-
-//     for (int i = 0; i < y; i++) {
-//         tmp = tmp * x;
-//     }
-
-//     return tmp;
-// }
-
-// int arrayToInt(int* list) {
-//     int sum = 0;
-//     for(int i = 0; i < 7; i++) {
-//         sum = sum + list[i] * pow(2, 7 - i);
-//     }
-//     return sum;
-// }
-
-// int* hexToBinary(char* hexString) {
-//     int* list = (int*)malloc(8 * sizeof(int)); // Dynamically allocate memory
-//     switch(hexString[0]) {
-//         case('0'):
-//             //Already 0
-//             break;
-//         case('1'):
-//             list[3] = 1;
-//             break;
-//         case('2'):
-//             list[2] = 1;
-//             break;
-//         case('3'):
-//             list[3] = 1;
-//             list[2] = 1;
-//             break;
-//         case('4'):
-//             list[1] = 1;
-//             break;
-//         case('5'):
-//             list[1] = 1;
-//             list[3] = 1;
-//             break;
-//         case('6'):
-//             list[1] = 1;
-//             list[2] = 1;
-//             break;
-//         case('7'):
-//             list[1] = 1;
-//             list[2] = 1;
-//             list[3] = 1;
-//             break;
-//         case('8'):
-//             list[0] = 1;
-//             break;
-//         case('9'):
-//             list[0] = 1;
-//             list[3] = 1;
-//             break;
-//         case('A'):
-//             list[0] = 1;
-//             list[2] = 1;
-//             break;
-//         case('B'):
-//             list[0] = 1;
-//             list[2] = 1;
-//             list[3] = 1;
-//             break;
-//         case('C'):
-//             list[0] = 1;
-//             list[1] = 1;
-//             break;
-//         case('D'):
-//             list[0] = 1;
-//             list[1] = 1;
-//             list[3] = 1;
-//             break;
-//         case('E'):
-//             list[0] = 1;
-//             list[1] = 1;
-//             list[2] = 1;
-//             break;
-//         case('F'):
-//             list[0] = 1;
-//             list[1] = 1;
-//             list[2] = 1;
-//             list[3] = 1;
-//             break;
-//         default:
-//             break;
-//     }
-
-//     switch(hexString[1]) {
-//         case('0'):
-//             //Already 0
-//             break;
-//         case('1'):
-//             list[7] = 1;
-//             break;
-//         case('2'):
-//             list[6] = 1;
-//             break;
-//         case('3'):
-//             list[7] = 1;
-//             list[6] = 1;
-//             break;
-//         case('4'):
-//             list[5] = 1;
-//             break;
-//         case('5'):
-//             list[5] = 1;
-//             list[7] = 1;
-//             break;
-//         case('6'):
-//             list[5] = 1;
-//             list[6] = 1;
-//             break;
-//         case('7'):
-//             list[5] = 1;
-//             list[6] = 1;
-//             list[7] = 1;
-//             break;
-//         case('8'):
-//             list[4] = 1;
-//             break;
-//         case('9'):
-//             list[4] = 1;
-//             list[7] = 1;
-//             break;
-//         case('A'):
-//             list[4] = 1;
-//             list[6] = 1;
-//             break;
-//         case('B'):
-//             list[4] = 1;
-//             list[6] = 1;
-//             list[7] = 1;
-//             break;
-//         case('C'):
-//             list[4] = 1;
-//             list[5] = 1;
-//             break;
-//         case('D'):
-//             list[4] = 1;
-//             list[5] = 1;
-//             list[7] = 1;
-//             break;
-//         case('E'):
-//             list[4] = 1;
-//             list[5] = 1;
-//             list[6] = 1;
-//             break;
-//         case('F'):
-//             list[4] = 1;
-//             list[5] = 1;
-//             list[6] = 1;
-//             list[7] = 1;
-//             break;
-//         default:
-//             break;
-//     }
-//     return list;
-// }
-
-// int hexStringtoInteger(char* hexString, int stringLength) {
-//     return 0;
-// }
-
-// void displayReceived(char* hexString, int stringLength) {
-//     printf("\n===========================================================");
-//     printf("\n      Beginning to display the incoming message!\n");
-
-//     //Start: Getting the hexString broken into the different parts
-//     char charVersion[2];
-//     char charcommID[4];
-//     char charlengthType[2];
-//     char charCheckSum[2];
-
-//     // for(int i = 0; i < stringLength-3; i++){
-//     //     printf("%c",hexString[i]);
-//     // }
-
-//     charVersion[0] = hexString[0]; 
-//     charVersion[1] = hexString[1];
-
-//     charcommID[0] = hexString[2];
-//     charcommID[1] = hexString[3];
-//     charcommID[2] = hexString[4];
-//     charcommID[3] = hexString[5];
-    
-//     charlengthType[0] = hexString[6];
-//     charlengthType[1] = hexString[7];
-    
-//     charCheckSum[0] = hexString[stringLength - 2];
-//     charCheckSum[1] = hexString[stringLength - 1];
-
-//     //Finding the length of the payload
-//     int length = arrayToInt(hexToBinary(charlengthType));
-//     int type = hexToBinary(charlengthType)[7];
-//     //STOP: Getting the hexString broken into the different parts
-
-//     char hexPair[4] = {'0','x',charVersion[0], charVersion[1]};
-//     int version = strtol(hexPair, NULL, 16);
-//     printf("Version: %d\n", version);
-
-
-
-
-
-
-//     char hexPairA[4] = {'0','x',charcommID[0], charcommID[1]};
-//     int a = strtol(hexPairA, NULL, 16);
-
-//     char hexPairb[4] = {'0','x',charcommID[2], charcommID[3]};
-//     int b = strtol(hexPairb, NULL, 16);
-
-//     printf("CommID: ");
-//     printf("%d%d\n",b,a);
-
-
-
-
-
-
-//     printf("length: %d\n", length);
-//     printf("Type: %d\n", type);
-
-
-//     printf("Payload: ");
-
-//     for(int i = 4; i < 32+(length+8); i+=2){
-
-//         char hexPair[4] = {'0','x',hexString[i], hexString[i + 1]};
-//         char asciiChar = strtol(hexPair, NULL, 16);
-
-//         printf("%c",(char)asciiChar);
-//     }
-
-
-
-//     printf("\nMessage Checksum: ");
-
-//     char hexPairc[4] = {'0','x',hexString[stringLength-7], hexString[stringLength-6]};
-//     int c = strtol(hexPairc, NULL, 16)<<4;
-
-//     char hexPaird[4] = {'0','x',hexString[stringLength-5], hexString[stringLength-4]};
-//     int d = strtol(hexPaird, NULL, 16);
-
-//     int cSum = c+d;
-//     printf("%d\n",cSum);
-    
-//     int calculatedCheckSum = checkSum(hexString, stringLength-4);
-//     printf("Calculated Checksum: %d\n\n",calculatedCheckSum);
-
-
-
-//     printf("===========================================================\n");
-
-//     if(calculatedCheckSum != cSum){
-//         printf("INVALID CHECKSUM, SENDING REQUEST AGAIN");
-//         main();
-//     }
-// }

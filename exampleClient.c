0bb411da064b46af55ce7ffc1548d481ab53d33b
Ben McDaniel <47250557+Ben-McDaniel@users.noreply.github.com>
2024-01-22 21:14:43 -0500

    a

--- exampleClient.c
+++ exampleClient.c
@@ -65,37 +65,60 @@ int main() {
     }
 
     uint8_t msg1[] = {0x09, 0xA8, 0x00, 0x05, 0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x00, 0xB2, 0x80};
-    uint8_t msg2[] = {0x09, 0x00, 0x76, 0x60, 0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x00, 0xCF, 0x6C};
+    uint8_t msg2[] = {0x09, 0xA, 0x00, 0x60, 0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x00, 0xCF, 0x6C};
     uint8_t msg3[] = {0x09, 0x74, 0x18, 0x41, 0x31, 0x20, 0x76, 0x18, 0x37, 0x12};
     uint8_t msg4[] = {0x09, 0x01, 0x18, 0x35, 0x31, 0x20, 0x76, 0x03, 0x37, 0xA6};
 
 
+    //iterate through messages with each main call
+    if(messageCount == 1){
+        printf("Sending RHP Message: hello\n");
+        if (sendto(clientSocket, msg1, sizeof(msg1), 0,
+                (struct sockaddr *) &serverAddr, sizeof (serverAddr)) < 0) {
+            perror("sendto failed");
+            return 0;
+        }
 
-    // if(messageCount == 1){
-    //     printf("Sending RHP Message: hello\n");
-    //     uint8_t msg1[] = {0x09, 0xA8, 0x00, 0x05, 0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x00, 0xB2, 0x80};
-    // }else if(messageCount == 2){
-    //     printf("Sending RHP Message: hi\n");
-    //     uint8_t msg2[] = {0x09, 0xA8, 0x00, 0x05, 0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x00, 0xB2, 0x80};
-    // }else if(messageCount == 3){
-    //     printf("Sending RHMP Message of type: Message_Request\n");
-    //     uint8_t msg3[] = {0x12, 0xE8, 0x30, 0x41, 0x31, 0x20, 0x76, 0x18, 0x15, 0x9E};
-    // }else{
-    //     printf("Sending RHMP Message of type: ID_Request\n");
-    //     uint8_t msg4[] = {0x12, 0xE8, 0x30, 0x41, 0x31, 0x20, 0x76, 0x18, 0x15, 0x9E};
-    // }
-    
-    if (sendto(clientSocket, msg4, sizeof(msg4), 0,
-            (struct sockaddr *) &serverAddr, sizeof (serverAddr)) < 0) {
-        perror("sendto failed");
-        return 0;
+        /* Receive message from server */
+        nBytes = recvfrom(clientSocket, buffer, BUFSIZE, 0, NULL, NULL);
+        displayMessage(buffer, nBytes);
+    }else if(messageCount == 2){
+        printf("Sending RHP Message: hi\n");
+        if (sendto(clientSocket, msg2, sizeof(msg2), 0,
+                (struct sockaddr *) &serverAddr, sizeof (serverAddr)) < 0) {
+            perror("sendto failed");
+            return 0;
+        }
+
+
+        /* Receive message from server */
+        nBytes = recvfrom(clientSocket, buffer, BUFSIZE, 0, NULL, NULL);
+        displayMessage(buffer, nBytes);
+    }else if(messageCount == 3){
+        printf("Sending RHMP Message of type: Message_Request\n");
+        if (sendto(clientSocket, msg3, sizeof(msg3), 0,
+                (struct sockaddr *) &serverAddr, sizeof (serverAddr)) < 0) {
+            perror("sendto failed");
+            return 0;
+        }
+
+
+        /* Receive message from server */
+        nBytes = recvfrom(clientSocket, buffer, BUFSIZE, 0, NULL, NULL);
+        displayMessage(buffer, nBytes);
+    }else{
+        printf("Sending RHMP Message of type: ID_Request\n");
+                if (sendto(clientSocket, msg4, sizeof(msg4), 0,
+                (struct sockaddr *) &serverAddr, sizeof (serverAddr)) < 0) {
+            perror("sendto failed");
+            return 0;
+        }
+
+
+        /* Receive message from server */
+        nBytes = recvfrom(clientSocket, buffer, BUFSIZE, 0, NULL, NULL);
+        displayMessage(buffer, nBytes);
     }
-
-
-    /* Receive message from server */
-    nBytes = recvfrom(clientSocket, buffer, BUFSIZE, 0, NULL, NULL);
-    displayMessage(buffer, nBytes);
-
 
     close(clientSocket);
     return 0;
@@ -106,14 +129,17 @@ void messageSendSuccess(){
 }
 
 void displayMessage(char* buffer, int nBytes){
+    // for(int i = 0; i < nBytes; i+=2){
+    //     printf("\n%x %x\n", (uint8_t)buffer[i],(uint8_t)buffer[i+1]);
 
+    // }
     printf("==================================\n");
     printf("Message Recieved\n");
     printf("    RHP Version: %d\n",buffer[0]);
     printf("    commID: %u%u\n",(uint8_t)buffer[2],(uint8_t)buffer[1]);
 
-    uint8_t length = (uint8_t)buffer[3]<<1;
-    length = length>>1;
+    uint8_t length = (uint8_t)buffer[3]&0b01111111;
+
     printf("    Length: %d\n", length);
 
     uint8_t type = (uint8_t)buffer[3]&0b10000000;
@@ -142,7 +168,7 @@ void displayMessage(char* buffer, int nBytes){
         messageSendSuccess();
     }
     
-    // main();
+    main();
 }
 
 void printPayload(char* buffer, int length){
@@ -157,9 +183,9 @@ void printPayload(char* buffer, int length){
 void printPayloadRHMP(char* buffer, int length){
     //know that the payload starts on buffer[4]
 
-    for(int i = 0; i < length; i+=2){
-        printf("\n%x %x\n", (uint8_t)buffer[i],(uint8_t)buffer[i+1]);
-    }
+    // for(int i = 0; i < length; i+=2){
+    //     printf("\n%x %x\n", (uint8_t)buffer[i],(uint8_t)buffer[i+1]);
+    // }
     printf("    RHMP Protocol:\n");
 
     int dstPort = (buffer[4]<<4) + (buffer[5]>>4);
